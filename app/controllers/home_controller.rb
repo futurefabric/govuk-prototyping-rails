c0c3fe4ecc1cc8ef55679ebd3e4e800dcf839a98
Guy Moorhouse <gmoorhouse@gmail.com>
2014-02-20 13:10:29 +0000

    Removing home route Redundant as a starting point. Set examples as new root

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def index
-		@title = 'GOV.UK prototype'
-		@has_breadcrumbs = true
-		@has_related_links = true
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ GovukPrototypingRails::Application.routes.draw do
   
   get "examples/index"
   get "examples" => 'examples#index'
-  get "home/index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'examples#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
