1b43b9311af81a28b0dda9bd202f4d8b906dfa0d
Guy Moorhouse <gmoorhouse@gmail.com>
2014-05-22 16:49:34 +0100

    Continued work on travel advice prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,34 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.sticky
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#chapter-nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top + 75 <= scrollPos) {
+          $('#chapter-nav ol li a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
+
+  if($("#chapter-nav").offset().top + $("#chapter-nav").height() > $("footer").offset().top) {
+
+  }
+}
+
+$(document).ready(function(){
+  if($("#chapter-nav").length !== -1) {
+    $("#chapter-nav").sticky({topSpacing:20});
+    $(document).on("scroll", onScroll);
+  }
+});
