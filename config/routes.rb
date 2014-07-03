8cb76e695ffb61be104f1428edc75a30400f0c53
Guy Moorhouse <gmoorhouse@gmail.com>
2014-07-03 15:38:18 +0100

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 GovukPrototypingRails::Application.routes.draw do
-  
+
   get "embassies/index"
   get "examples/index"
   get "examples" => 'examples#index'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'examples#index'
+  root 'embassies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
