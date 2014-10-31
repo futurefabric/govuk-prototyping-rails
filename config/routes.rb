2f077de5d1debd7d001a26286d98fcac2232238b
Guy Moorhouse <gmoorhouse@gmail.com>
2014-10-31 14:47:56 +0000

    Change root url

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ GovukPrototypingRails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'mhra#index'
+  root 'mhra#interstitial'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
